e16ec97f3f1e38100580236529978087eae62a5f
PatronDenis <adz225cs@gmail.com>
2024-03-09 15:26:33 +0000

    localizaçãp pt-PT aprofundada

---
+++ quartz/i18n/locales/pt-PT.ts
@@ -0,0 +1,83 @@
+import { Translation } from "./definition"
+
+export default {
+  propertyDefaults: {
+    title: "Sem título",
+    description: "Nenhuma descrição disponibilizada",
+  },
+  components: {
+    callout: {
+      note: "Nota",
+      abstract: "Sumário",
+      info: "Info",
+      todo: "Todo",
+      tip: "Dica",
+      success: "Successo",
+      question: "Questão",
+      warning: "Aviso",
+      failure: "Falha",
+      danger: "Perigo",
+      bug: "Bug",
+      example: "Examploe",
+      quote: "Citação",
+    },
+    backlinks: {
+      title: "Backlinks",
+      noBacklinksFound: "No backlinks found",
+    },
+    themeToggle: {
+      lightMode: "Light mode",
+      darkMode: "Dark mode",
+    },
+    explorer: {
+      title: "Explorer",
+    },
+    footer: {
+      createdWith: "Criado com",
+    },
+    graph: {
+      title: "Vista de gráfico",
+    },
+    recentNotes: {
+      title: "Notas recentes",
+      seeRemainingMore: ({ remaining }) => `Ver ${remaining} mais →`,
+    },
+    transcludes: {
+      transcludeOf: ({ targetSlug }) => `Transclude de ${targetSlug}`,
+      linkToOriginal: "Ligação ao original",
+    },
+    search: {
+      title: "Pesquisar",
+      searchBarPlaceholder: "Algo a pesquisar",
+    },
+    tableOfContents: {
+      title: "Tabela de conteúdo",
+    },
+    contentMeta: {
+      readingTime: ({ minutes }) => `${minutes} minutos de leitura`,
+    },
+  },
+  pages: {
+    rss: {
+      recentNotes: "Notas recentes",
+      lastFewNotes: ({ count }) => `Últimas ${count} notas`,
+    },
+    error: {
+      title: "Não encontrado",
+      notFound: "Ou esta página é privada ou não existe",
+    },
+    folderContent: {
+      folder: "Pasta",
+      itemsUnderFolder: ({ count }) =>
+        count === 1 ? "1 item dentro desta pasta." : `${count} items dentro desta pasta.`,
+    },
+    tagContent: {
+      tag: "Etiqueta",
+      tagIndex: "Índice de Etiqueta",
+      itemsUnderTag: ({ count }) =>
+        count === 1 ? "1 item com esta etiqueta." : `${count} items com esta etiqueta.`,
+      showingFirst: ({ count }) => `A mostrar as primeiras ${count} etiquetass.`,
+      totalTags: ({ count }) => `Encontradas ${count} etiquetas totais.`,
+    },
+  },
+} as const satisfies Translation
